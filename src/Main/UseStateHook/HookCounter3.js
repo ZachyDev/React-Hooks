0ab9a954781e706c488c2a470b762d8a8b23cecc
ZachyDev <mosetizachary001@gmail.com>
2020-05-17 16:21:54 +0300

    updated useState hook

--- src/Main/UseStateHook/HookCounter3.js
+++ src/Main/UseStateHook/HookCounter3.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react'
 
 function HookCounter3() {
-    const [user, setUser] = useState({ firstName: '', lastName: '' })
+    const [user, setUser] = useState({ firstName: '',lastName: '' })
     return (
         <form>
-            <input 
-             type="text"  
+            <input
+             type="text"
              value = { user.firstName }
-             onChange = { e => setUser({...user,firstName: e.target.value })}
+             onChange = { e => setUser({...user,firstName: e.target.value})}
              />
-             <input 
-             type="text"  
+             <input
+             type="text"
              value = { user.lastName }
-             onChange = { e => setUser({...user,lastName: e.target.value })}
+             onChange = { e => setUser({...user,lastName: e.target.value})}
              />
              <h2>FirstName - { user.firstName }</h2>
-             <h2>LastName  - { user.lastName }</h2>
-             { JSON.stringify(user) }
+             <h2>LastName - { user.lastName }</h2>
+             <strong>{ JSON.stringify(user)}</strong>
         </form>
     )
 }
--- src/Main/UseStateHook/HookCounter4.js
+++ src/Main/UseStateHook/HookCounter4.js
@@ -3,18 +3,18 @@ import React, { useState } from 'react'
 function HookCounter4() {
     const [items, setItems] = useState([])
     const addItem = () => {
-        setItems([...items,{
-            id: items.length,
+        setItems([...items, {
+            id: items.id,
             value: Math.floor(Math.random() * 10 ) + 1
         }])
     }
     return (
         <div>
-            <button onClick = { addItem }>Add Item</button>
+            <button onClick = { addItem }>Add Items</button>
             <ul>
-                {
-                    items.map(item => <li key = { item.id }> { item.value }</li>)
-                }
+              {
+                  items.map(item=> <li key = { item.id }>{ item.value }</li>)
+              }
             </ul>
         </div>
     )
